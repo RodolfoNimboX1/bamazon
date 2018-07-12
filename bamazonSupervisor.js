1f2a6e644865f86fab702c746931eea8bfceec19
Rodolfo Resines <rodolfo.sresines@gmail.com>
2018-07-11 20:34:13 -0500

    finished the supervisor app to display the table

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,6 +1,7 @@
 // Connection to Database
 // ======================================================
 var cTable = require('console.table');
+var Table = require("easy-table");
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
@@ -48,22 +49,22 @@ function runSupervisor() {
 // View Product Sales by Department
 // ======================================================
 function depSales() {
-    var query = "SELECT departments.department_id, departments.department_name, departments.over_head_costs, products.product_sales, products.department_name " ;
-    query += "FROM departments INNER JOIN products ON (departments.department_name = products.department_name) ";
-    query += "ORDER BY departments.department_id ";
+    var query = "SELECT departments.department_id AS id, departments.department_name AS name, departments.over_head_costs AS cost, SUM(products.product_sales) AS sales, products.department_name, SUM(products.product_sales) - departments.over_head_costs AS profit FROM products RIGHT JOIN departments ON (products.department_name = departments.department_name) GROUP BY departments.department_id "; 
     connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.log('');
+	    console.log('Bamazon Products Sales by Department');
+        console.log('');
         for (var i = 0; i < res.length; i++) {
-            console.table([{
-                Deparment_Id: res[i].department_id,
-                Department_Name: res[i].department_name,
-                Department_Costs: res[i].over_head_costs,
-                Product_Sales: res[i].product_sales
-                }]
-            )
-        }
+            console.log("Department ID: " + res[i].id +
+            " || Department Name: " + res[i].name + 
+            " || OverHead Costs: " + res[i].cost + 
+            " || Total Sales: " + res[i].sales + 
+            "|| Total Profit: " + res[i].profit)
+        };
+        runSupervisor();
     }); // query ends
-// depSales ends
-}
+}// depSales ends
 
 // Create New Department
 // ======================================================
